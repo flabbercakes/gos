e4822c2477e8d58788a479847ce7d42910a64520
flabbercakes <flabbercakes.duxzz.sunny@gmail.com>
2016-09-30 18:03:36 +0800

    Update sinisteramumu.lua

--- sinisteramumu.lua
+++ sinisteramumu.lua
@@ -1,112 +1,70 @@
-if GetObjectName(GetMyHero()) ~= "Amumu" then return end	
+if GetObjectName(myHero) ~= "Amumu" then return end
 
-require("OpenPredict")
+require('OpenPredict')
 
-local AmumuMenu = Menu("Amumu", "Amumu")						
-AmumuMenu:SubMenu("Combo", "Combo")							
-AmumuMenu.Combo:Boolean("Q", "Use Q", true)						
-AmumuMenu.Combo:Boolean("W", "Use W", true)						
-AmumuMenu.Combo:Boolean("R", "Use R", true)						
-AmumuMenu.Combo:Boolean("KSQ", "Killsteal with Q", true)		
-AmumuMenu.Combo:Boolean("UOP", "Use OpenPredict for R", true)
-Amumu.c:Slider("RE", "Enemies around for R", 3, 1, 5, 2)
+print("Sinister Amumu Loaded")
 
-AmumuMenu:SubMenu("ks", "Killsteal")
-AmumuMenu.ks:Boolean("KSS","Smart Killsteal", true)
-AmumuMenu.ks:Boolean("KSQ","Killsteal with Q", true)
-AmumuMenu.ks:Boolean("KSE","Killsteal with E", true)
+local AmumuMenu = Menu("JaxNation", "JaxNation")
 
-AmumuMenu:SubMenu("p", "Prediction")
-AmumuMenu.p:Slider("hQ", "HitChance Q", 20, 60, 100, 80)
+AmumuMenu:Menu("Combo", "Combo")
+AmumuMenu.Combo:Boolean("useQ", "Use Q", true)
+AmumuMenu.Combo:Boolean("useW", "Use W", true)
+AmumuMenu.Combo:Boolean("useE", "Use E", true)
+AmumuMenu.Combo:Boolean("useR", "Use R", true)
 
-AmumuMenu:SubMenu("d", "Draw Damage")
-AmumuMenu.d:Boolean("dD","Draw Damage", true)
-AmumuMenu.d:Boolean("dQ","Draw Q", true)
-AmumuMenu.d:Boolean("dW","Draw W", true)
-AmumuMenu.d:Boolean("dE","Draw E", true)
-AmumuMenu.d:Boolean("dR","Draw R", true)
+AmumuMenu:SubMenu("a", "AutoLvl")
+AmumuMenu.a:Boolean("aL", "Use AutoLvl", true)
+AmumuMenu.a:DropDown("aLS", "AutoLvL", 1, {"Q-W-E","Q-E-W"})
+AmumuMenu.a:Slider("sL", "Start AutoLvl with LvL x", 1, 1, 18, 1)
+VMenu.a:Boolean("hL", "Humanize LvLUP", true
 
-function Mode()
-    if _G.IOW_Loaded and IOW:Mode() then
-        return IOW:Mode()
-        elseif _G.PW_Loaded and PW:Mode() then
-        return PW:Mode()
-        elseif _G.DAC_Loaded and DAC:Mode() then
-        return DAC:Mode()
-        elseif _G.AutoCarry_Loaded and DACR:Mode() then
-        return DACR:Mode()
-        elseif _G.SLW_Loaded and SLW:Mode() then
-        return SLW:Mode()
+--Spells
+local AmumuQ = { range = 1100}
+local AmumuW = { range = 300}
+local AmumuE = { range = 350}
+local AmumuR = { range = 550}
+
+--Start
+OnTick(function(myHero)
+    if not IsDead(myHero) then
+        local target = GetCurrentTarget()
+
+        --Combo
+        if IOW:Mode() == "Combo" then
+            --Q
+            if AmumuMenu.Combo.useQ:Value() and Ready(_Q) and ValidTarget(target, GetRange(myHero) + GetHitBox(target)) then
+                CastSkillShot(_Q)
+            end
+            --W
+            if AmumuMenu.Combo.useW:Value() and Ready(_W) and ValidTarget(target, AmumuW.range) then
+                CastTargetSpell(target,_W)
+            end
+            --E
+            if AmumuMenu.Combo.useW:Value() and Ready(_E) and ValidTarget(target, GetRange(myHero)) then
+                CastSpell(_E)
+            end
+            --R
+            if AmumuMenu.Combo.useR:Value() and Ready(_R) and EnemiesAround(myHero, 600) >= 1 then
+                CastSpell(_R)
+            end
+        end
     end
+end)
 
-
-OnTick(function()									--The code inside the Function runs every tick
-	
-	local target = GetCurrentTarget()					--Saves the "best" enemy champ to the target variable
-		
-	if IOW:Mode() == "Combo" then						--Check if we are in Combo mode (holding space)
-			
-		
-				if AmumuMenu.Combo.Q:Value() and Ready(_Q) and ValidTarget(target, 550) then		
-			if not AmumuMenu.Combo.UOP:Value() then			
-				local QPred = GetPredictionForPlayer(GetOrigin(myHero), target, GetMoveSpeed(target), math.huge, 10, 1100, 10, true, true)
-				if QPred.HitChance == 1 then		
-					CastSkillShot(_Q,QPred.PredPos)			
-				end		
-				local QPred = GetCircularAOEPrediction(target,AmumuQ)	
-				if RPred.hitChance > 0.2 then							
-					CastSkillShot(_Q,QPred.castPos)						
-				end
-			end
-	
-		if AmumuMenu.Combo.W:Value() and Ready(_W) and ValidTarget(target, 300) then
-			local targetPos = GetOrigin(target)		--saves the XYZ coordinates of the target to the variable
-			CastSpell(_W)			--Since the W is a skillshot (select area), we have to cast it at a point on the ground (targetPos)
-		end		--Ends the W logic
-	
-			if AmumuMenu.Combo.E:Value() and Ready(_E) and ValidTarget(target, 350) then
-			local targetPos = GetOrigin(target)		
-			CastSpell(_E)			
-		end		
-		
-		if AmumuMenu.Combo.R:Value() and Ready(_R) and ValidTarget(target, 550) then		
-			if not AmumuMenu.Combo.UOP:Value() then			
-				local RPred = GetPredictionForPlayer(GetOrigin(myHero), target, GetMoveSpeed(target), math.huge, 2,  550,  550, false, true)
-				if RPred.HitChance == 1 then		
-					CastSkillShot(_R,RPred.PredPos)			
-				end		
-			else		
-				local RPred = GetCircularAOEPrediction(target,AnnieR)	
-				if RPred.hitChance > 0.2 then							
-					CastSkillShot(_R,RPred.castPos)						
-				end
-			end
-		end	
-	end		
-	
-	--KillSteal
-	for _, enemy in pairs(GetEnemyHeroes()) do
-		if AmumuMenu.KillSteal.KSQ:Value() and Ready(_Q) and ValidTarget(enemy, 1100) then
-			if QDmg(enemy) >= GetCurrentHP(enemy) then
-				CastTargetSpell(enemy, _Q)
-			end
-		end
-
-		if AmumuMenu.KillSteal.KSE:Value() and Ready(_E) and ValidTarget(enemy, 350) then
-			if EDmg(enemy) >= GetCurrentHP(enemy) then
-				CastSpell(_E)
-			end
-		end
-
-	--AutoR
-	for _, enemy in pairs(GetEnemyHeroes()) do
-		if AmumuMenu.Misc.AR:Value() and Ready(_R) and ValidTarget(enemy, 550) and EnemiesAround(enemy, 300) >= MalphiteMenu.Misc.ARC:Value() then
-			local RPred = GetCircularAOEPrediction(target,RStats)
-			if RPred.hitChance >= 0.3 then 
-				CastSkillShot(_R,RPred.castPos)
-			end	
+function lvlUp()
+	if VMenu.a.aL:Value() and GetLevelPoints(myHero) >= 1 and GetLevel(myHero) >= VMenu.a.sL:Value() then
+		if VMenu.a.hL:Value() then
+			DelayAction(function() LevelSpell(lTable[VMenu.a.aLS:Value()][GetLevel(myHero)-GetLevelPoints(myHero)+1]) end, math.random(0.500,0.750))
+		else
+			LevelSpell(lTable[VMenu.a.aLS:Value()][GetLevel(myHero)-GetLevelPoints(myHero)+1])
 		end
 	end
 end
 
-print("Thank You For Using Sinister Amumu")
+
+AmumuMenu:SubMenu("s", "Skin Changer")
+  skinMeta = {["Amumu"] = {"Classic", "Emumu", "Almost-Prom King", "Pharaoh", "Surprise Party", "Re-Gifted", "Vancouver", "Sad Robot", "Little Knight""}}
+  AmumuMenu.s:DropDown('skin', myHero.charName.. " Skins", 1, skinMeta[myHero.charName],function(model)
+        HeroSkinChanger(myHero, model - 1) print(skinMeta[myHero.charName][model] .." ".. myHero.charName .. " Loaded!") 
+    end,
+true)
